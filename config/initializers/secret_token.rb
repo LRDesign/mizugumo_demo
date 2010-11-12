adcb403a08079bfa6a1c7198502a5d3899bacd62
Evan Dorn <evan@lrdesign.com>
2010-11-12 11:44:19 -0600

    initial version of bare app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NinjaHelperDemo::Application.config.secret_token = '85d96dface7955b8872adce9639d8d67ec510c0acb2e4718c18ce8dfc073ab7f64829b93d46dda7cf334d5f8d1a8d8c16ca9ad19e9946091aa47a1947adc548d'
