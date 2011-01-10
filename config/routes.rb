28d3ba414491c03a04b6a473202083ed043a7758
Evan Dorn <evan@lrdesign.com>
2011-01-10 14:24:05 -0800

    added another field to product, removed index.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 NinjaHelperDemo::Application.routes.draw do
   resources :products
+  root :to => 'products#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- db/migrate/20101112203054_create_products.rb
+++ db/migrate/20101112203054_create_products.rb
@@ -2,8 +2,8 @@ class CreateProducts < ActiveRecord::Migration
   def self.up
     create_table :products do |t|
       t.string :name
+      t.text   :description
       t.decimal :price
-
       t.timestamps
     end
   end
